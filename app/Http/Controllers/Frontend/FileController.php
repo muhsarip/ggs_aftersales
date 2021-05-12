95d04f2777d6ded6c7d4bc98c64566b961f6484a
Muhamad Syarif H <syarif@centrinova.com>
2021-05-12 14:50:45 +0700

    update

--- app/Http/Controllers/Frontend/FileController.php
+++ app/Http/Controllers/Frontend/FileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Intervention\Image\Facades\Image;
+use Image;
 
 class FileController extends Controller
 {
